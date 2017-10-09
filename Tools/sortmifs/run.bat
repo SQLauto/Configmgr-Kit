1d921f94730eb2c530ea51301b67bccb5dd97447
Eswar Koneti <eskonr@gmail.com>
2017-10-09 18:29:19 +0800

    Add files via upload

--- Tools/sortmifs/run.bat
+++ Tools/sortmifs/run.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
 
-cd D:\Media\12374sortmifs
-F:
-sortmifs dbserver=PRODSCM dbname=SMS_KBS xls=F:\eswar2.xls mifdir=E:\SCCM\inboxes\auth\dataldr.box\BADMIFS /S /DEL
+cd G:\Sources\Tools\sortmifs
+G:
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\BADMIFS_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS /S /DEL
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\DeltaMismatch_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS\DeltaMismatch /S /DEL
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\ErrorCode_4_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS\ErrorCode_4 /S /DEL
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\MajorMismatch_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS\MajorMismatch /S /DEL
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\NonExistentRow_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS\NonExistentRow /S /DEL
+sortmifs.exe dbserver=VSGRHOWPSCP01 dbname=CM_RT1 xls=G:\Sources\Tools\sortmifs\Outdated_%date:~-10,2%%date:~-7,2%%date:~-4,4%.xls mifdir=G:\Configmgr\inboxes\auth\dataldr.box\BADMIFS\Outdated /S /DEL
 
-Pause
 
